dad1d06d8b0d2e455334c9f323c14eec14e34a6b
Pierluigi Viti <pierluigi@commercelayer.io>
2021-12-10 16:10:54 +0100

    fix: Authentication with multiple scopes

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -4,27 +4,26 @@ import { Authenticate } from '#typings'
 
 const authenticate: Authenticate = async (type, credentials, scope, code) => {
   const auth = createAuth(credentials)
-  let r = null
-  const s: any = {
-    body: {
-      scope,
-    },
+  let authRes = null
+  const opts: any = {
+    scopes: scope,
   }
   if (type === 'clientCredentials') {
-    r = await auth.credentials.getToken(s)
+    authRes = await auth.credentials.getToken(opts)
   }
   if (type === 'owner') {
-    r = await auth.owner.getToken(
+    authRes = await auth.owner.getToken(
       credentials.username as string,
       credentials.password as string,
-      s
+      opts
     )
   }
   if (type === 'authorizationCode') {
     const uri = auth.code.getUri()
-    r = await authorizationCode(auth, uri, code)
+    authRes = await authorizationCode(auth, uri, code)
   }
-  return r
+  console.log(authRes)
+  return authRes
 }
 
 export default authenticate
