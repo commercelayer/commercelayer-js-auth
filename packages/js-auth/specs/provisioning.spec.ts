fce6fff6f2d0d79e136d60164fb6321587956741
Pierluigi Viti <pierluigi@commercelayer.io>
2023-10-27 09:58:16 +0200

    feat: add provisioning authentication

---
+++ packages/js-auth/specs/provisioning.spec.ts
@@ -0,0 +1,24 @@
+import { provisioning } from 'src'
+
+const clientId = process.env.VITE_TEST_PROVISIONING_CLIENT_ID
+const clientSecret = process.env.VITE_TEST_PROVISIONING_CLIENT_SECRET
+const domain = process.env.VITE_TEST_PROVISIONING_DOMAIN
+
+describe('Provisioning', () => {
+  it('Get a provisioning token', async () => {
+    const res = await provisioning.authentication({
+      domain,
+      clientId,
+      clientSecret
+    })
+   
+    expect(res).toHaveProperty('accessToken')
+    expect(res).toHaveProperty('tokenType')
+    expect(res).toHaveProperty('expiresIn')
+    expect(res).toHaveProperty('scope')
+    expect(res).toHaveProperty('createdAt')
+    expect(res).toHaveProperty('expires')
+    expect(res.expires).toBeInstanceOf(Date)
+    expect(res.expires.getTime()).toBeGreaterThan(Date.now())
+  })
+})
