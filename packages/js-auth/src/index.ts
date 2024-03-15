c47495b480306f4a68d9f08d65a739d8d0ea002c
Pierluigi Viti <pierluigi@commercelayer.io>
2024-03-15 10:08:51 +0100

    fix: fix missing provisioning types

--- packages/js-auth/src/index.ts
+++ packages/js-auth/src/index.ts
@@ -6,3 +6,8 @@ export type * from './types/authorizationCode.js'
 export type * from './types/clientCredentials.js'
 export type * from './types/password.js'
 export type * from './types/refreshToken.js'
+
+export type {
+  TProvisioningOptions,
+  TProvisioningReturn
+} from './provisioning.js'
