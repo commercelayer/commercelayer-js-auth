9b0a6a9e98a3cf269dc0a7fa5915f633de252f9d
Marco Montalbano <marco.montalbano@commercelayer.io>
2024-04-16 22:37:41 +0200

    deps: remove unused dependency

--- packages/js-auth/vitest.config.ts
+++ packages/js-auth/vitest.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vitest/config'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
     globals: true
-  },
-  plugins: [tsconfigPaths()]
+  }
 })
