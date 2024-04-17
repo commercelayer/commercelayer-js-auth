9fbee64b912abfaf708ea74d5e70c012213a019f
Marco Montalbano <marco.montalbano@commercelayer.io>
2024-04-17 12:26:11 +0200

    chore: update examples

--- examples/deno/index.ts
+++ examples/deno/index.ts
@@ -1,4 +1,4 @@
-import { authenticate } from 'https://esm.sh/@commercelayer/js-auth'
+import { authenticate } from 'jsr:@commercelayer/js-auth'
 
 const auth = await authenticate('client_credentials', {
   clientId: 'BISG8bb3GWpC8_D7Nt1SuWWdieS5bJq831A50LgB_Ig',
