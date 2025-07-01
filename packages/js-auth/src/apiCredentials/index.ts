5466102d40562cd3973afd83b0d817f1efc3064a
Marco Montalbano <marco.montalbano@commercelayer.io>
2025-07-01 16:54:00 +0200

    chore: add comments to public methods

--- packages/js-auth/src/apiCredentials/index.ts
+++ packages/js-auth/src/apiCredentials/index.ts
@@ -27,25 +27,25 @@ export function makeIntegration(
   store: Omit<StoreOptions, "customerStorage">,
 ): {
   options: SetRequired<AuthOptions, "clientSecret">
+  /**
+   * Get the current integration authorization.
+   *
+   * This will return the authorization from memory or storage.
+   * If the authorization is not found or expired, it will fetch a new one.
+   */
   getAuthorization: () => Promise<ApiCredentialsAuthorization>
+  /**
+   * Revoke the current integration authorization.
+   *
+   * This will remove the authorization from memory and storage, and revoke the access token.
+   */
   revokeAuthorization: () => Promise<RevokeReturn>
 } {
   const auth = makeAuth(options, store, true)
 
   return {
     options,
-    /**
-     * Get the current integration authorization.
-     *
-     * This will return the authorization from memory or storage.
-     * If the authorization is not found or expired, it will fetch a new one.
-     */
     getAuthorization: auth.getAuthorization,
-    /**
-     * Revoke the current integration authorization.
-     *
-     * This will remove the authorization from memory and storage, and revoke the access token.
-     */
     revokeAuthorization: async () => {
       const { ownerType: type, accessToken } = await auth.getAuthorization()
 
@@ -76,30 +76,35 @@ export function makeSalesChannel(
   store: StoreOptions,
 ): {
   options: Omit<AuthOptions, "clientSecret">
+  /**
+   * Get the current sales channel authorization.
+   *
+   * This will return the authorization from memory or storage.
+   * If the authorization is not found or expired, it will fetch a new one.
+   * IF the customer is logged in with a `refreshToken`, it will also attempt to refresh the token when expired.
+   */
   getAuthorization: () => Promise<ApiCredentialsAuthorization>
+  /**
+   * Sets the customer authorization.
+   * This will store the authorization in memory and storage.
+   * It will also validate the access token to ensure it contains a valid customer owner.
+   *
+   * The option `refreshToken` is optional and can be used to set a refresh token for the customer (e.g. "remember me" functionality).
+   * It will be used to refresh the access token when it expires.
+   */
   setCustomer: (options: StorageValue) => Promise<ApiCredentialsAuthorization>
+  /**
+   * Logs out the current customer.
+   *
+   * This will remove the customer authorization from memory and storage, and revoke the access token.
+   */
   logoutCustomer: () => Promise<RevokeReturn>
 } {
   const auth = makeAuth(options, store)
 
   return {
     options,
-    /**
-     * Get the current sales channel authorization.
-     *
-     * This will return the authorization from memory or storage.
-     * If the authorization is not found or expired, it will fetch a new one.
-     * IF the customer is logged in with a `refreshToken`, it will also attempt to refresh the token when expired.
-     */
     getAuthorization: auth.getAuthorization,
-    /**
-     * Sets the customer authorization.
-     * This will store the authorization in memory and storage.
-     * It will also validate the access token to ensure it contains a valid customer owner.
-     *
-     * The option `refreshToken` is optional and can be used to set a refresh token for the customer (e.g. "remember me" functionality).
-     * It will be used to refresh the access token when it expires.
-     */
     setCustomer: async (options) => {
       const decodedJWT = jwtDecode(options.accessToken)
 
@@ -111,11 +116,6 @@ export function makeSalesChannel(
         "The provided access token does not contain a valid customer owner.",
       )
     },
-    /**
-     * Logs out the current customer.
-     *
-     * This will remove the customer authorization from memory and storage, and revoke the access token.
-     */
     logoutCustomer: async () => {
       const { ownerType: type, accessToken } = await auth.getAuthorization()
 
